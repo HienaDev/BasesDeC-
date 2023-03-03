981bab75f88ffc9cdef7a42b2374b3b3b66935f1
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-03 14:49:50 +0000

    Added exercise 1

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -6,7 +6,17 @@ namespace Exercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            uint a = 0xFEDE;
+            string a_bin = Convert.ToString(a, 2);
+            Console.WriteLine(String.Format("Decimal: {0}, em binário é {1}", a, a_bin));
+
+
+            a &= 0x0F0F;
+
+            Console.WriteLine();
+
+            Console.WriteLine(a);
+            Console.WriteLine(String.Format("{0:x}".ToUpper(), a));
         }
     }
 }
